b0634ba50ea4011a598a528e40448d78b5a4631d
artem-romanovich <75304507+artem-romanovich@users.noreply.github.com>
2022-06-23 13:51:17 +0700

    Update APIService.java

--- app/src/main/java/com/artrom/flychat/notification/APIService.java
+++ app/src/main/java/com/artrom/flychat/notification/APIService.java
@@ -8,7 +8,7 @@ import retrofit2.http.POST;
 public interface APIService {
     @Headers({
             "Content-Type:application/json",
-            "Authorization:key=AAAACO5Bfsw:APA91bGMWDhgllAhGfhDSyXXzjRZGTtHHcJuNkCnNn5Jk9Qb8BWBe3Xx2eMHr3OJ19nVfRmneHemG1PWZ_BfL_P3tyibKspqn_zkMWOe_GLZFvUi0wPdKo9YqI8xV3tTmCsfNyK5_0Ep"
+            "Authorization:key=secret"
     })
     @POST("fcm/send")
     Call<MyResponse> sendNotification(@Body Sender body);
